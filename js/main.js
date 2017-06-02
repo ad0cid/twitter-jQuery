0b8093436fda32be0fff88e89cf38f6ff0ee0bb9
ad0cid <ad0raci0ncid@gmail.com>
2017-06-02 10:51:23 -0500

    agregue estilos de botones y la funcion de boton desactivado

--- js/main.js
+++ js/main.js
@@ -1,8 +1,55 @@
+// ESTE SI FUNCIONA
 $(document).ready(function(){
-	$("textarea").change(function(){
+	$("textarea").keyup(function(){
+		var $textoMostrar = $("textarea").val();
+		var caracteres = $textoMostrar.length;
+		console.log(caracteres);
+		if(caracteres != 0){
+			$("button").removeAttr("disabled");
+		}
+		else {
+			$("button").attr("disabled");
+		}
 		$("button").click(function(){
-			var $textoMostrar = $("textarea").val();
-			$("p").append($textoMostrar);
+			$("p").text($textoMostrar);
 		});
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+// $(document).ready(function(){
+// 	$("textarea").change(function(){
+// 		var $caracteres = $("textarea").val();
+// 		console.log($caracteres);
+// 		var $numeroCaracteres = Number($caracteres.length);
+// 		console.log($numeroCaracteres);
+
+// 		if($numeroCaracteres < 140){
+// 			$("button").attr("disable");
+// 		}
+// 		else if($numeroCaracteres>=120){
+// 			$("button").addClass("btn120");
+// 		}
+// 		else if($numeroCaracteres>=130 && $numeroCaracteres<140){
+// 			$("button").addClass("btn130");
+// 		}
+// 	});
+// });
